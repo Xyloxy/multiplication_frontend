28f7e5a9d1949b09ac931d6bb1172859cc19a385
Xylox <seba15833@gmail.com>
2024-11-11 19:39:01 +0100

    Fix missing const when creating app

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
-app = createApp(App)
+const app = createApp(App)
 app.use(VueAxios, axios)
 
 app.mount('#app')
